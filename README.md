f1ea6c6bfe0d5330f0fb7b4fa5e8c4d0d7769514
熊静松 <8238536+xiong-jingsong@user.noreply.gitee.com>
2021-07-24 12:59:52 +0800

    add READM

---
+++ README.md
@@ -0,0 +1,12 @@
+### install
+```bash
+yarn global add aslan-translation
+or
+npm i -g aslan-translation
+```
+
+### how to use
+```bash
+fy 内容
+//content
+```
--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,8 @@
+#!/usr/bin/env node
 import * as commander from "commander";
 import { translation } from './main'
-import * as querystring from 'querystring'
 
 const program = new commander.Command()
-const x = querystring.stringify({ q: 'new', sign: '1234' })
-console.log(x)
 program.version('0.0.1')
     .name('fy')
     .usage('<English or zh>')
