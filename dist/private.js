2ae99a86149db477b490f959957877968a92ed3a
熊静松 <8238536+xiong-jingsong@user.noreply.gitee.com>
2021-07-24 13:00:40 +0800

    删除不必要的文件

--- dist/private.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.token = exports.appid = void 0;
-var appid = '20210331000756037';
-exports.appid = appid;
-var token = 'l3Zs5J7q_TyyUoRoay6L';
-exports.token = token;
